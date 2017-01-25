e14dfe22704d0ad7b74c92744c0f73a2af773ee8
Ezequiel Rosas <nhtzr.rg@gmail.com>
2017-01-24 21:46:38 -0600

    add filter example

---
+++ src/main/java/mx/nhtzr/osgiee/web/internal/MyFilter.java
@@ -0,0 +1,41 @@
+package mx.nhtzr.osgiee.web.internal;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.ObjectWriter;
+import org.codehaus.jackson.map.SerializationConfig;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import java.io.IOException;
+
+/**
+ * Created by hero on 1/24/2017.
+ */
+@Component("myFilter")
+public class MyFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    private static final Log log = LogFactory.getLog(MyFilter.class);
+    private final ObjectWriter objectWriter = new ObjectMapper()
+            .disable(SerializationConfig.Feature.FAIL_ON_EMPTY_BEANS)
+            .writerWithDefaultPrettyPrinter();
+
+    @Override
+    public void filter(ContainerRequestContext context) throws IOException {
+        log.info("request = " + json(context));
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        log.info("response = " + json(responseContext));
+    }
+
+    private String json(Object o) throws IOException {
+        return objectWriter.writeValueAsString(o);
+    }
+
+}
